3aa8fdffdbe0927752b2b1bf16f4c9f281d70e6c
Atulsak <sakhareat@gmail.com>
2022-12-30 23:26:31 +0530

    Added: UC4 ability to insert with index

--- src/com/linkedlist/Linkedlist.java
+++ src/com/linkedlist/Linkedlist.java
@@ -4,7 +4,7 @@ package com.linkedlist;
 
 public class Linkedlist {
 	
-	 Node head;
+	  Node head;
 	    class Node {
 	        String data; // initialising data in first location
 	        Node next;   // initialising next in node class
@@ -50,14 +50,70 @@ public class Linkedlist {
 	            tempNode = tempNode.next;                       // Storing next node in temp data
 	        }
 	    }
-	    
+
+	    // Counting the size
+	    public int size() {
+	        int count = 0;
+
+	        Node temp = head;
+
+	        while (temp != null) {
+	            count++;
+	            temp = temp.next;
+	        }
+
+	        return count;
+
+	    }
+	        // inserting the data in between
+	    public void insertIndex(int userIndex, String data) {
+	        Linkedlist obj1 = new Linkedlist();
+
+	        // User trying to insert a first position
+	        if (userIndex == 0)
+	            obj1.addFirst(data);
+
+	            // User trying to insert a last position
+	        else if (userIndex == size())
+	            obj1.addLast(data);
+
+	            // User trying to insert a invalid position
+	        else if (userIndex < 0 || userIndex >= size())
+	            System.out.println("Invalid index");
+
+	            // User trying to insert a specific index
+	        else {
+	            // Creating New Node
+	            Node newNode = new Node(data);
+
+	            // To track traversing
+	            int index = 0;
+
+	            // Pointers to track left & right side elements
+	            Node left = head;
+	            Node right = left.next;
+
+	            // Traverse or Move till last element before user entered index
+	            while (index < userIndex - 1) {
+	                left = left.next;
+	                right = right.next;
+	                index++;
+	            }
+	            // Connecting new Node with right side elements
+	            newNode.next = right;
+
+	            // Connecting left side elements with new Node
+	            left.next = newNode;
+	        }
+	    }
+
 	    public static void main(String[] args) {
 	        Linkedlist obj = new Linkedlist();
 
 	        obj.addFirst("56");
 	        obj.addFirst("30");
 	        obj.addFirst("70");
-
+	        obj.insertIndex(2, "Your");
 	        obj.Print();
 	    }
 }
