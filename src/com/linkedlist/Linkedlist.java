5d237fb98c8ed61c2d9425c3832e7563a112cb90
Atulsak <sakhareat@gmail.com>
2022-12-30 23:32:03 +0530

    Added: UC6 Ability to delete last

--- src/com/linkedlist/Linkedlist.java
+++ src/com/linkedlist/Linkedlist.java
@@ -4,126 +4,146 @@ package com.linkedlist;
 
 public class Linkedlist {
 	
-	  Node head;
-	    class Node {
-	        String data; // initialising data in first location
-	        Node next;   // initialising next in node class
+	Node head;
+    class Node {
+        String data; // initialising data in first location
+        Node next;   // initialising next in node class
 
-	        Node(String data){
-	            this.data = data;
-	            this.next = null;
-	        }
-	    }
-	    //Adding the data in first position
-	    public void addFirst(String data){
-	        Node newNode = new Node(data); // Making object of node class
-	        if(head == null){              // Checking if linked list is empty
-	            head = newNode;
-	            return;
-	        }
-	        newNode.next = head;
-	        head = newNode;
-	    }
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    //Adding the data in first position
+    public void addFirst(String data){
+        Node newNode = new Node(data); // Making object of node class
+        if(head == null){              // Checking if linked list is empty
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
 
-	    // Adding the data to the last
-	    public void addLast(String data){
-	        Node newNode = new Node(data);
-	        if(head == null){
-	            head = newNode;
-	            return;
-	        }
-	        Node tempNode = head;
-	        while(tempNode.next != null){
-	            tempNode= tempNode.next;
-	        }
-	        tempNode.next = newNode;
-	    }
-	    //Printing the code
-	    void Print() {
-	        if (head == null) {                                  // Checking f head is empty or not
-	            System.out.println("Linked list is empty...");
-	            return;
-	        }
-	        Node tempNode = head;                                 // Taking temp node
-	        while (tempNode != null) {                          // Running the temp node throughout the linked list
-	            System.out.print(tempNode.data + "-");         // Printing temp node data
-	            tempNode = tempNode.next;                       // Storing next node in temp data
-	        }
-	    }
+    // Adding the data to the last
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node tempNode = head;
+        while(tempNode.next != null){
+            tempNode= tempNode.next;
+        }
+        tempNode.next = newNode;
+    }
+    //Printing the code
+    void Print() {
+        if (head == null) {                                  // Checking f head is empty or not
+            System.out.println("Linked list is empty...");
+            return;
+        }
+        Node tempNode = head;                                 // Taking temp node
+        while (tempNode != null) {                          // Running the temp node throughout the linked list
+            System.out.print(tempNode.data + "-");         // Printing temp node data
+            tempNode = tempNode.next;                       // Storing next node in temp data
+        }
+    }
 
-	    // Counting the size
-	    public int size() {
-	        int count = 0;
+    // Counting the size
+    public int size() {
+        int count = 0;
 
-	        Node temp = head;
+        Node temp = head;
 
-	        while (temp != null) {
-	            count++;
-	            temp = temp.next;
-	        }
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
 
-	        return count;
+        return count;
 
-	    }
-	        // inserting the data in between
-	    public void insertIndex(int userIndex, String data) {
-	        Linkedlist obj1 = new Linkedlist();
+    }
+        // inserting the data in between
+    public void insertIndex(int userIndex, String data) {
+        Linkedlist obj1 = new Linkedlist();
 
-	        // User trying to insert a first position
-	        if (userIndex == 0)
-	            obj1.addFirst(data);
+        // User trying to insert a first position
+        if (userIndex == 0)
+            obj1.addFirst(data);
 
-	            // User trying to insert a last position
-	        else if (userIndex == size())
-	            obj1.addLast(data);
+            // User trying to insert a last position
+        else if (userIndex == size())
+            obj1.addLast(data);
 
-	            // User trying to insert a invalid position
-	        else if (userIndex < 0 || userIndex >= size())
-	            System.out.println("Invalid index");
+            // User trying to insert a invalid position
+        else if (userIndex < 0 || userIndex >= size())
+            System.out.println("Invalid index");
 
-	            // User trying to insert a specific index
-	        else {
-	            // Creating New Node
-	            Node newNode = new Node(data);
+            // User trying to insert a specific index
+        else {
+            // Creating New Node
+            Node newNode = new Node(data);
 
-	            // To track traversing
-	            int index = 0;
+            // To track traversing
+            int index = 0;
 
-	            // Pointers to track left & right side elements
-	            Node left = head;
-	            Node right = left.next;
+            // Pointers to track left & right side elements
+            Node left = head;
+            Node right = left.next;
 
-	            // Traverse or Move till last element before user entered index
-	            while (index < userIndex - 1) {
-	                left = left.next;
-	                right = right.next;
-	                index++;
-	            }
-	            // Connecting new Node with right side elements
-	            newNode.next = right;
+            // Traverse or Move till last element before user entered index
+            while (index < userIndex - 1) {
+                left = left.next;
+                right = right.next;
+                index++;
+            }
+            // Connecting new Node with right side elements
+            newNode.next = right;
 
-	            // Connecting left side elements with new Node
-	            left.next = newNode;
-	        }
-	    }
+            // Connecting left side elements with new Node
+            left.next = newNode;
+        }
+    }
 
-	    // Deleting the first element
-	    public void deleteFirst(){
-	        if(head == null){
-	            System.out.println(" the list is empty");
-	            return;
-	        }
-	        head = head.next;
-	    }
+    // Deleting the first element
+    public void deleteFirst(){
+        if(head == null){
+            System.out.println(" the list is empty");
+            return;
+        }
+        head = head.next;
+    }
 
-	    public static void main(String[] args) {
-	        Linkedlist obj = new Linkedlist();
+    // deleting the last element
+    public void deleteLast(){
+        if(head == null){
+            System.out.println(" the list is empty");
+            return;
+        }
+        if(head.next == null){
+            head= null;
+            return;
+        }
+        Node secoundLast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secoundLast = secoundLast.next;
+        }
+        secoundLast.next= null;
+    }
 
-	        obj.addFirst("56");
-	        obj.addFirst("30");
-	        obj.addFirst("70");
-	        obj.insertIndex(2, "Your");
-	        obj.deleteFirst();
-	        obj.Print();
-	    }
+    public static void main(String[] args) {
+        Linkedlist obj = new Linkedlist();
+
+        obj.addFirst("56");
+        obj.addFirst("30");
+        obj.addFirst("70");
+        obj.insertIndex(2, "Your");
+        obj.deleteFirst();
+        obj.deleteLast();
+        obj.Print();
+    }
 }
