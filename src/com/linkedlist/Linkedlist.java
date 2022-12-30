17a4d801d53b17059c08ecde250ed8e989d973b1
Atulsak <sakhareat@gmail.com>
2022-12-30 23:21:43 +0530

    Added: UC3 Ablility to add at the last

--- src/com/linkedlist/Linkedlist.java
+++ src/com/linkedlist/Linkedlist.java
@@ -1,13 +1,13 @@
 package com.linkedlist;
 
-import java.util.LinkedList;
+
 
 public class Linkedlist {
 	
 	 Node head;
 	    class Node {
-	        String data; 
-	        Node next;   
+	        String data; // initialising data in first location
+	        Node next;   // initialising next in node class
 
 	        Node(String data){
 	            this.data = data;
@@ -16,8 +16,8 @@ public class Linkedlist {
 	    }
 	    //Adding the data in first position
 	    public void addFirst(String data){
-	        Node newNode = new Node(data); 
-	        if(head == null){              
+	        Node newNode = new Node(data); // Making object of node class
+	        if(head == null){              // Checking if linked list is empty
 	            head = newNode;
 	            return;
 	        }
@@ -25,13 +25,39 @@ public class Linkedlist {
 	        head = newNode;
 	    }
 
+	    // Adding the data to the last
+	    public void addLast(String data){
+	        Node newNode = new Node(data);
+	        if(head == null){
+	            head = newNode;
+	            return;
+	        }
+	        Node tempNode = head;
+	        while(tempNode.next != null){
+	            tempNode= tempNode.next;
+	        }
+	        tempNode.next = newNode;
+	    }
+	    //Printing the code
+	    void Print() {
+	        if (head == null) {                                  // Checking f head is empty or not
+	            System.out.println("Linked list is empty...");
+	            return;
+	        }
+	        Node tempNode = head;                                 // Taking temp node
+	        while (tempNode != null) {                          // Running the temp node throughout the linked list
+	            System.out.print(tempNode.data + "-");         // Printing temp node data
+	            tempNode = tempNode.next;                       // Storing next node in temp data
+	        }
+	    }
+	    
 	    public static void main(String[] args) {
 	        Linkedlist obj = new Linkedlist();
 
-	        obj.addFirst("70");
-	        obj.addFirst("30");
 	        obj.addFirst("56");
+	        obj.addFirst("30");
+	        obj.addFirst("70");
 
-
-	}
+	        obj.Print();
+	    }
 }
